d078f71ca4f86368c2a60cabb77d6e971c6f997f
Zeeshan-2k1 <ashraf2k1zeeshan@gmail.com>
2022-04-03 13:14:02 +0530

    New portfolio

---
+++ src/data/ProjectData.js
@@ -0,0 +1,47 @@
+// blog data
+
+export const Projects = [
+  {
+    id: 1,
+    name: 'Build multi user white board Website using React and NodeJs',
+    tags: ['react', 'gsap', 'styled-components'],
+    date: '16 Sept, 2021',
+    imgSrc:
+      'https://play-lh.googleusercontent.com/vlO4VHErW6ZKSRHkTu2GA9qi-DuYkO3sxKw5wlV6_kpyzFEwGZ5zLQaPpRLqNuOmeMo',
+    link: 'https://github.com/Zeeshan-2k1/Drawing-Tool',
+  },
+  {
+    id: 2,
+    name: 'Build a streaming web app using React',
+    tags: ['react', 'styled-components'],
+    date: '7 Oct, 2020',
+    imgSrc:
+      'https://www.seasiainfotech.com/blog/wp-content/uploads/2020/09/Things-to-Keep-in-Mind-While-Developing-a-Live-Video-Streaming-Website-like-Twitch-Seasia-Infotech.png',
+    link: 'https://github.com/Zeeshan-2k1/Streams',
+  },
+  {
+    id: 3,
+    name: 'Build a real time path tracking website using react-native and google api',
+    tags: ['react', 'react-lottie', 'styled-components'],
+    date: '27 Oct, 2020',
+    imgSrc: 'https://i.ytimg.com/vi/1cOIm7n5V54/maxresdefault.jpg',
+    link: 'https://github.com/Zeeshan-2k1/Tracks',
+  },
+  {
+    id: 4,
+    name: 'Build a chat application using React and Node',
+    tags: ['react', 'redux', 'framer-motion'],
+    date: '26 Aug, 2020',
+    imgSrc:
+      'https://media.istockphoto.com/vectors/chat-app-logo-sms-messenger-label-design-for-mobile-app-online-with-vector-id1281337303?k=20&m=1281337303&s=170667a&w=0&h=w0p8XTLDkub6dhBfDIOLdiNwOOFucpI-nfgE4vLM6i0=',
+    link: 'https://github.com/Zeeshan-2k1/ChatRoom',
+  },
+  {
+    id: 5,
+    name: 'Build Website for college club',
+    tags: ['react', 'reactJS', 'web-development'],
+    date: '',
+    imgSrc: 'https://blog.hnccbits.com/img/avatar-icon.png',
+    link: 'https://github.com/hnccbits/Website',
+  },
+];
---
+++ src/data/WorkData.js
@@ -0,0 +1,38 @@
+// work data
+
+export const Work = [
+  {
+    id: 1,
+    name: 'Wingify: (JavaScript Developer)',
+    description:
+      'Build a web performance analysis tool using lighthouse and pupeeteer',
+    tags: [
+      'react',
+      'lighthouse',
+      'puppeteer',
+      'web-vitals',
+      'peroformance',
+      'a/b testing',
+    ],
+
+    visit: 'https://wingify.com/',
+  },
+  {
+    id: 2,
+    name: 'La Polo: (Frontend Intern)',
+    description:
+      'I worked as a web developer in django and react frameworks. Worked 2 major projects where my major task was to build the frontend and connect with the backend',
+    tags: ['react', 'django', 'components', 'tailwind css'],
+
+    visit: 'http://lapolo.in/',
+  },
+  {
+    id: 3,
+    name: 'Cadworks Pvt. Ltd: (Full Stack Intern)',
+    description:
+      'A Job listing website build in React and AWS Amplify. With searching and filtering functionalities. Build a mobile app for the same. Also build a admin panel for CMS',
+    tags: ['react', 'sass', 'aws', 'react-native', 'serverless app', 'graphql'],
+
+    visit: 'http://react-job-listing-website.codebucks27.vercel.app/',
+  },
+];
